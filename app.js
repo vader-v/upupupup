754783d032949b9dcf55a5bd57aab2bab174a207
amirb <amir.bullock9@gmail.com>
2023-07-22 09:17:56 -0400

    add background sprite for game

--- app.js
+++ app.js
@@ -6,6 +6,24 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const gravity = 1;
+
+class Sprite {
+  constructor({ position, imageSrc }) {
+    this.position = position;
+    this.image = new Image();
+    this.image.src = imageSrc;
+  }
+  
+  draw() {
+    if (!this.image) return;
+    c.drawImage(this.image, this.position.x, this.position.y);
+  }
+
+  update() {
+    this.draw();
+  }
+}
+
 class Player {
   constructor(position) {
     this.position = position;
@@ -57,10 +75,19 @@ const keys = {
   }
 };
 
+const background = new Sprite({
+  position: {
+    x: 0,
+    y: 0
+  },
+  imageSrc: './images/kirbylands.png'
+})
+
 function animate() {
   window.requestAnimationFrame(animate);
   c.fillStyle = 'white';
   c.fillRect(0, 0, canvas.width, canvas.height);
+  background.update();
   player.update();
   // player2.update();
 
